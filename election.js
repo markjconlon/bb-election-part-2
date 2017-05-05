9921cee06a63e81707854e851fe73903cf5d48f2
Mark <markjconlon@gmail.com>
2017-05-05 10:15:23 -0400

    pair programmed this with Margarita on her machine this is my copy

---
+++ election.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+
+//Margarita and I's solution to bikini bottoms part 2
+
+  $.ajax({
+    url: 'https://bb-election-api.herokuapp.com/',
+    method: 'GET',
+    dataType: 'json'
+  }).done(function(data){
+
+    for (var i = 0; i < data.candidates.length; i++) {
+      candidate = data.candidates[i];
+      var cInfo = candidate.name + ': ' + candidate.votes + ' votes.';
+      var voteForm = '<form class="vote-form" method="post" action="https://bb-election-api.herokuapp.com/vote"><input type="hidden" name="name" value="' + candidate.name + '"><button type="submit">VOTE</button></form>';
+
+      var item = $('<li>').html(cInfo + voteForm);
+      $('#candidate-list').append(item);
+
+    }
+  });
+
+  $('body').on('submit', '.vote-form', function(e){
+    e.preventDefault();
+    var that = $(this).children('input[type=hidden]').val();
+    $.ajax({
+      url: $(this).attr('action'),
+      method: $(this).attr('method'),
+      data: { name: $(this).children('input[type=hidden]').val()},
+      dataType: 'json'
+    }).done(function(data){
+      console.log(data);
+    }).always(function(){
+      console.log(that);
+    });
+  });
+
+});
